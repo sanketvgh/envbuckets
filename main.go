a9cca25d9976e1b2bf86c33c10a164311e9b0a88
Sanket Vaghela <84759786+sanketvgh@users.noreply.github.com>
2026-09-12 17:38:51 +0530

    💄 style: show banner and alpha warning on bare invocation

--- main.go
+++ main.go
@@ -13,11 +13,27 @@ var (
 	date    = "unknown"
 )
 
+const banner = `
+                 _                _        _
+  ___ _ ____   _| |__  _   _  ___| | _____| |_ ___
+ / _ \ '_ \ \ / / '_ \| | | |/ __| |/ / _ \ __/ __|
+|  __/ | | \ V /| |_) | |_| | (__|   <  __/ |_\__ \
+ \___|_| |_|\_/ |_.__/ \__,_|\___|_|\_\___|\__|___/
+`
+
 func main() {
 	if len(os.Args) > 1 && (os.Args[1] == "--version" || os.Args[1] == "version") {
 		fmt.Printf("envbuckets %s (%s, %s)\n", version, commit, date)
 		return
 	}
-	fmt.Fprintln(os.Stderr, "envbuckets: not implemented yet")
-	os.Exit(3)
+
+	fmt.Print(banner)
+	fmt.Println()
+	fmt.Println("Your .env switches branches with you.")
+	fmt.Println()
+	fmt.Println("Hello Sanket.")
+	fmt.Println()
+	fmt.Println("WARNING: early alpha, under active development.")
+	fmt.Println("The CLI is not implemented yet. Commands and config may change without notice until v1.0.")
+	fmt.Printf("\nversion %s\n", version)
 }
